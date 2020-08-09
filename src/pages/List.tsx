74cc9649e5a81c8796c46236f9669e57c3349e75
marco-sut <sut.marco@gmail.com>
2020-08-09 13:14:04 +0200

    [UBS] Loader component

--- src/pages/List.tsx
+++ src/pages/List.tsx
@@ -4,12 +4,13 @@ import styles from '../styles/modules/list.module.scss';
 import { StateContext, Action, ActionTypes } from '../lib/context/State';
 import { State } from '../model/State';
 import UserApi from '../lib/api/UserApi';
+import Loader from '../lib/ui/Loader';
 
 const List = () => {
   const [data, func] = useContext(StateContext);
-  const { loading, error, users } = data as State;
+  const { loading, users } = data as State;
   const dispatch = func as (action: Action) => {};
-  
+
   useEffect(() => {
     const fetchData = async () => {
       const users = await UserApi.loadUser();
@@ -19,18 +20,22 @@ const List = () => {
         data: { users }
       });
     }
-    
+
     fetchData();
   }, [dispatch]);
 
   return (
-    <article className={styles.list}>
-      {loading}
-      {error}
-      {users.map((user) => {
-        return <UserCard key={user.id} id={user.id} name={user.name} email={user.email} />
-      })}
-    </article>
+    <>
+      {loading ? <Loader /> : (
+        <article className={styles.list}>
+          {
+            users.map((user) => {
+              return <UserCard key={user.id} id={user.id} name={user.name} email={user.email} />
+            })
+          }
+        </article>
+      )}
+    </>
   );
 };
 
